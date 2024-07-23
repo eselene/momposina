085d4eea763f52c8d3d494c69ab32023bcee447d
eselene <eviougeas>
2024-07-23 12:03:02 +0200

    MAJ produits OK

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,16 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use App\Entity\Produit;
 use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/')]
 // #[Route('/produit')]
@@ -20,8 +20,16 @@ class ProduitController extends AbstractController
     #[Route('/', name: 'app_produit_index', methods: ['GET'])]
     public function index(ProduitRepository $produitRepository): Response
     {
+        $produits = $produitRepository->findAll();
+        $deleteForms = [];
+        
+        foreach ($produits as $produit) {
+            $deleteForms[$produit->getId()] = $this->createDeleteForm($produit->getId())->createView();
+        }
+
         return $this->render('produit/index.html.twig', [
-            'produits' => $produitRepository->findAll(),
+            'produits' => $produits,
+            'delete_forms' => $deleteForms,
         ]);
     }
 
@@ -31,34 +39,33 @@ class ProduitController extends AbstractController
         $produit = new Produit();
         $form = $this->createForm(ProduitType::class, $produit);
         $form->handleRequest($request);
-    
+
         if ($form->isSubmitted() && $form->isValid()) {
             $imageFile = $form->get('photo1')->getData();
-    
+
             if ($imageFile) {
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
-    
+
                 try {
                     $imageFile->move(
                         $this->getParameter('images_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    $this->addFlash('error', 'Un erreur est apparu pendant le téléchargement du fichier.');
+                    $this->addFlash('error', 'Une erreur est apparue pendant le téléchargement du fichier.');
                 }
                 // Enregistre seulement le nom du fichier dans la base de données
-    
                 $produit->setPhoto1($newFilename);
             }
-    
+
             $entityManager->persist($produit);
             $entityManager->flush();
-    
+
             return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
         }
-    
+
         return $this->render('produit/new.html.twig', [
             'produit' => $produit,
             'form' => $form,
@@ -74,16 +81,35 @@ class ProduitController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'], requirements: ['id' => '\d+'])]
-    public function edit(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Produit $produit, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ProduitType::class, $produit);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('photo1')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('error', 'Une erreur est apparue pendant le téléchargement du fichier.');
+                }
+                $produit->setPhoto1($newFilename);
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
         }
+
         return $this->render('produit/edit.html.twig', [
             'produit' => $produit,
             'form' => $form,
@@ -93,11 +119,19 @@ class ProduitController extends AbstractController
     #[Route('/{id}', name: 'app_produit_delete', methods: ['POST'])]
     public function delete(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $produit->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$produit->getId(), $request->request->get('_token'))) {
             $entityManager->remove($produit);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('app_produit_delete', ['id' => $id]))
+            ->setMethod('DELETE')
+            ->getForm();
     }
 }
--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -5,15 +5,15 @@ namespace App\Form;
 use App\Entity\Produit;
 use App\Entity\SousCategorie;
 use App\Entity\User;
-use Symfony\Component\Validator\Constraints\File;
+// use Symfony\Component\Validator\Constraints\File;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Repository\SousCategorieRepository;
 // use App\Form\EntityRepository;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProduitType extends AbstractType
@@ -21,17 +21,17 @@ class ProduitType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, [ // Use TextType for product names
-                'label' => 'Nom *' // Clearer label for user
+            ->add('nom', TextType::class, [
+                'label' => 'Nom         *',
             ])
             ->add('nomEs', TextType::class, [ // Use TextType for Spanish name
                 'required' => false,
                 'label' => 'Nom (espagnol)' // Clearer label for user
             ])
-            ->add('description', TextType::class, [ // Use TextType for product description
-                'label' => 'Description *' // Clearer label for user
+            ->add('description', TextType::class, [
+                'label' => 'Description',
             ])
-            ->add('pays', TextType::class, [ // Use TextType for country
+            ->add('pays', TextType::class, [
                 'required' => false,
                 'label' => 'Pays d\'origine' // Clearer label for user
             ])
@@ -39,9 +39,6 @@ class ProduitType extends AbstractType
                 'required' => false,
                 'label' => 'Marque' // Clearer label for user
             ])
-            // ->add('prix', TextType::class, [ // Use TextType for price (likely a number)
-            //     'label' => 'Prix (€)' // Clearer label for user, assuming euros
-            // ])
             ->add('visibleWeb', CheckboxType::class, [ // Use CheckboxType for boolean visibility
                 'label' => 'Visible sur le site web' ,
                 'data' => true,
@@ -68,20 +65,18 @@ class ProduitType extends AbstractType
                 // make it optional so you don't have to re-upload the PDF file
                 // every time you edit the Product details
                 'required' => false,
-                // unmapped fields can't define their validation using attributes
-                // in the associated entity, so you can use the PHP constraint classes
+                'mapped' => false,
                 'constraints' => [
-                    new File([
-                        'maxSize' => '5M',
+                    new \Symfony\Component\Validator\Constraints\File([
+                        'maxSize' => '2M',
                         'mimeTypes' => [
                             'image/jpeg',
-                            'image/jpg',
                             'image/png',
                         ],
-                        'mimeTypesMessage' => 'Télécharger un fichier de type jpg, png, jpeg svp',
+                        'mimeTypesMessage' => 'Please upload a valid image file (JPEG or PNG)',
                     ])
                 ],
-            ])
+            ]);
             // Add fields for 'poids', 'unitePoids', 'ingredients', 'allergenes', and 'certification'
             // if needed, using appropriate form field types and labels
         ;
