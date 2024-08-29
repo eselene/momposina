7936dbf73b908fe5c1cc3be0ab814b1300b92906
eselene <eviougeas>
2024-08-29 15:56:20 +0200

    MAJ espace barre de recherche

--- src/Form/ProduitSearchType.php
+++ src/Form/ProduitSearchType.php
@@ -9,23 +9,25 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProduitSearchType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('query', TextType::class, [
                 'label' => 'Rechercher un produit',
-                'required' => false,                
+                'required' => false,
                 'attr' => [
                     'placeholder' => 'Rechercher',
-                    'class' => 'form-control',                     
+                    'class' => 'form-control', // Classe Bootstrap pour le champ de recherche
+                    'onkeydown' => 'if (event.key === "Enter") this.form.submit();' // Soumission sur Enter
+                ],
+                'label_attr' => [
+                    'class' => 'search-label mr-2' // Espacement avec la classe Bootstrap mr-2 (margin-right)
                 ],
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
+        $resolver->setDefaults([]);
     }
 }
