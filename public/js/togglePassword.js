f915b390b33e3701e5b46fd6436c89d79f48ec05
eselene <eviougeas>
2024-09-18 01:00:48 +0200

    login_register- toggle mdp dans file js

---
+++ public/js/togglePassword.js
@@ -0,0 +1,11 @@
+// public/js/togglePassword.js
+
+var mespasswords = document.querySelectorAll("input[type='password']");
+
+function togglePasswordVisibility() {
+  var allAreText = Array.from(mespasswords).every((input) => input.type === 'text');
+  mespasswords.forEach((input) => {
+    // Change le type du champ de mot de passe entre 'password' et 'text'
+    input.type = allAreText ? 'password' : 'text';
+  });
+}
