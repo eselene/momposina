fa3234510e418d15f8bcbe2924b3fc3549559beb
eselene <eviougeas>
2024-12-03 15:07:59 +0100

    galerie MAJ

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,7 +19,7 @@ use App\Form\ProduitSearchType;
 class MainController extends AbstractController
 {
     private const ALIM = 'Alimentation';
-    private const BOISSON = 'Boisson';  
+    private const BOISSON = 'Boisson';
     private const ELEMENTS_PAR_PAGE_E = 4; //evenement
     private const ELEMENTS_PAR_PAGE_B = 8; //alim et boisson
 
@@ -59,7 +59,7 @@ class MainController extends AbstractController
         // Crée le formulaire de recherche de produits
         $formResearch = $this->createForm(ProduitSearchType::class, null, ['method' => 'GET']);
         $formResearch->handleRequest($request);
-    
+
         $produits = $produitRepository->findBy(['sousCategorie' => $id, 'visibleWeb' => true]);
         if ($formResearch->isSubmitted() && $formResearch->isValid()) {
             $query = $formResearch->get('query')->getData();
@@ -68,24 +68,24 @@ class MainController extends AbstractController
                 $produits = $produitRepository->findByNomNomEs($query, $id);
             }
         }
-    
+
         // Récupère la sous-catégorie par son ID
         $sousCategorie = $sousCategorieRepository->find($id);
         // Récupère la description de la sous-catégorie, ou une chaîne vide si non trouvée
         $sousCategorieNom = $sousCategorie ? $sousCategorie->getDescription() : '';
-    
+
         // Paginate les résultats des produits
         $pagination = $paginator->paginate(
             $produits,
             $request->query->getInt('page', 1),
             self::ELEMENTS_PAR_PAGE_B
         );
-    
+
         // Vérifie si la page est pour une boisson, en ignorant la casse 
         $isPageForBoisson = strtolower($typePage) === 'boisson';
         // Sélectionne le template approprié en fonction du type de page
         $template = $isPageForBoisson ? 'main/boisson_detail.html.twig' : 'main/alimentation_detail.html.twig';
-    
+
         // Rend la vue avec les données nécessaires
         return $this->render($template, [
             'pagination' => $pagination,
@@ -94,7 +94,7 @@ class MainController extends AbstractController
             'pageTitle' => $typePage,
             'isBoisson' => $isPageForBoisson,
         ]);
-    }    
+    }
 
     #[Route('/alimentation/{id}', name: 'app_alimentation_detail', requirements: ['id' => '\d+'])]
     public function alimentationDetail(
@@ -132,36 +132,44 @@ class MainController extends AbstractController
     {
         $images = [
             [
-                'src' => 'images/galerie/barril.jpeg',
-                'alt' => 'photo de fûts de chêne et poupée'
+                'src' => 'images/galerie/barril.webp',
+                'alt' => 'photo de fûts de chêne et poupée',
+                'desc' => 'Plongez dans l\'ambiance festive et colorée de La Momposina Tienda Latina ! '
             ],
             [
-                'src' => 'images/galerie/alcoholFort.jpeg',
-                'alt' => 'photo d\'alcohol Fort'
+                'src' => 'images/galerie/alcoholFort.webp',
+                'alt' => 'photo d\'alcohol Fort',
+                'desc' => 'Découvrez notre large gamme de boissons exotiques et savourez un moment de détente. Chacune de nos bouteilles est une invitation à un voyage gustatif en Amérique Latine ! '
             ],
             [
-                'src' => 'images/galerie/cafe.jpeg',
-                'alt' => 'photo de fûts de café'
+                'src' => 'images/galerie/cafe.webp',
+                'alt' => 'photo de fûts de café',
+                'desc' => 'Laissez-vous emporter par l \'arôme envoûtant de nos cafés authentiques !'
             ],
             [
-                'src' => 'images/galerie/poupeeFaitMain.jpeg',
-                'alt' => 'photo de poupée fait main'
+                'src' => 'images/galerie/poupeeFaitMain.webp',
+                'alt' => 'photo de poupée fait main',
+                'desc' => 'Plongez dans notre univers et découvrez la richesse de notre culture à travers nos produits !'
             ],
             [
-                'src' => 'images/galerie/sacAmain.jpeg',
-                'alt' => 'photo de sacs à main'
+                'src' => 'images/galerie/sacAmain.webp',
+                'alt' => 'photo de sacs à main',
+                'desc' => 'Découvrez notre sélection de produits artisanaux faits main et uniques ! Venez vite, cette collection artisanale exceptionnelle est en exposition pour une durée limitée !'
             ],
             [
-                'src' => 'images/galerie/doudou.jpeg',
-                'alt' => 'photo de doudous'
+                'src' => 'images/galerie/doudou.webp',
+                'alt' => 'photo de doudous',
+                'desc' => 'Explorez notre boutique et trouvez des trésors cachés à chaque coin. Ne manquez pas cette exposition temporaire et plongez dans notre univers culturel !'
             ],
             [
-                'src' => 'images/galerie/trio.jpeg',
-                'alt' => 'photo d\'équipe momposina'
+                'src' => 'images/galerie/trio.webp',
+                'alt' => 'photo d\'équipe momposina',
+                'desc' => 'Venez à La Momposina et rencontrez notre équipe chaleureuse et accueillante. Découvrez nos délicieux plats et laissez-vous séduire par notre ambiance conviviale !'
             ],
             [
-                'src' => 'images/galerie/produits.jpeg',
-                'alt' => 'photo de produits'
+                'src' => 'images/galerie/produits.webp',
+                'alt' => 'photo de produits',
+                'desc' => 'Explorez notre large sélection de produits et boissons authentiques de l\'Amérique Latine !'
             ],
         ];
 
